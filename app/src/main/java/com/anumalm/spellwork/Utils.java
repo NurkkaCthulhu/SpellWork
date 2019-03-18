5ab000eeb81a355c77ae09dccb887a8da253cb0f
Anu Malm <nurkkacthulhu@gmail.com>
2019-03-18 22:42:21 +0200

    Utils.java: Add method hideSystemUI.
    Activities call this method to become fullscreen. Takes the activity's view as a parameter.

--- app/src/main/java/com/anumalm/spellwork/Utils.java
+++ app/src/main/java/com/anumalm/spellwork/Utils.java
@@ -1,10 +1,22 @@
 package com.anumalm.spellwork;
 
-
+import android.view.View;
 
 public final class Utils {
 
     private Utils(){}
 
+    public static void hideSystemUI(View v) {
 
+        v.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
