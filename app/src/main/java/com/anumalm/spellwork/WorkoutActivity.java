416d2cdb66e18f818fe7571c8a8b52ae4bc2590e
Anu Malm <nurkkacthulhu@gmail.com>
2019-03-18 22:48:08 +0200

    WorkoutActivity.java: Make the activity fullscreen, even after focus has been lost and regained.

--- app/src/main/java/com/anumalm/spellwork/WorkoutActivity.java
+++ app/src/main/java/com/anumalm/spellwork/WorkoutActivity.java
@@ -2,13 +2,25 @@ package com.anumalm.spellwork;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class WorkoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_workout);
+        View decorView = getWindow().getDecorView();
+        Utils.hideSystemUI(decorView);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = getWindow().getDecorView();
+            Utils.hideSystemUI(decorView);
+        }
     }
 
 }
