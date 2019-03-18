206c0841aa7e8aad540c0d41f742ffd1f46292c2
Anu Malm <nurkkacthulhu@gmail.com>
2019-03-18 22:43:42 +0200

    MainActivity.java: Move hideSystemUI-method to Utils-class and call it from there.

--- app/src/main/java/com/anumalm/spellwork/MainActivity.java
+++ app/src/main/java/com/anumalm/spellwork/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anumalm.spellwork;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,29 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        hideSystemUI();
+        View decorView = getWindow().getDecorView();
+        Utils.hideSystemUI(decorView);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            hideSystemUI();
+            View decorView = getWindow().getDecorView();
+            Utils.hideSystemUI(decorView);
         }
-    }
-    private void hideSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     public void workoutButton(View v) {
