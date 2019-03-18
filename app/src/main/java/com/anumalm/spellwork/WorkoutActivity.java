1dc438a1eb8280452ed13414c557899b270dfeda
Anu Malm <nurkkacthulhu@gmail.com>
2019-03-18 23:33:43 +0200

    WorkoutActivity.java: Add method for getting the View and hiding UI. Add method to getting SharedSettings and test them by putting the settings' texts to TextView.

--- app/src/main/java/com/anumalm/spellwork/WorkoutActivity.java
+++ app/src/main/java/com/anumalm/spellwork/WorkoutActivity.java
@@ -1,26 +1,44 @@
 package com.anumalm.spellwork;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class WorkoutActivity extends AppCompatActivity {
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
-        View decorView = getWindow().getDecorView();
-        Utils.hideSystemUI(decorView);
+        getViewAndHideUI();
+
+        t = findViewById(R.id.workoutText);
+        getSettings();
+
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            View decorView = getWindow().getDecorView();
-            Utils.hideSystemUI(decorView);
+            getViewAndHideUI();
         }
     }
+
+    private void getSettings() {
+        SharedPreferences settings = getSharedPreferences("UserSettings", 0);
+        String putThisToText = settings.getString("TestOne", "").toString();
+        putThisToText += settings.getString("TestTwo", "").toString();
+        t.setText(putThisToText);
+    }
+
+    private void getViewAndHideUI() {
+        View decorView = getWindow().getDecorView();
+        Utils.hideSystemUI(decorView);
+    }
+
 
 }
