089272c6601b1df87e71916508df1a95349c6bfc
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-24 12:57:12 +0200

    Utils.java: Add javadoc.

--- app/src/main/java/com/anumalm/spellwork/Utils.java
+++ app/src/main/java/com/anumalm/spellwork/Utils.java
@@ -2,10 +2,28 @@ package com.anumalm.spellwork;
 
 import android.view.View;
 
+/**
+ * Utils class holds useful methods that can be called from any other class.
+ *
+ * @author      Anu Malm     anu.malm@tuni.fi
+ * @version     2019.03.24
+ * @since       1.0
+ */
 public final class Utils {
 
+    /**
+     * Private constructor so that Utils-object cannot be instantiated.
+     */
     private Utils(){}
 
+    /**
+     * Sets the system UI visibility for the given view.
+     *
+     * After call, all the UI elements of Android are hidden unless dragged from the top,
+     * and after that they'll disappear on their own.
+     *
+     * @param v         View that we want to hide the UI from.
+     */
     public static void hideSystemUI(View v) {
 
         v.setSystemUiVisibility(
