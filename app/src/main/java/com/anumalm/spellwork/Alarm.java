3b80208ebb3b60d4036de73f2f39598c8cf9cb3e
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-24 20:44:56 +0200

    Alarm.java: Add vibration and custom sound to notification.

--- app/src/main/java/com/anumalm/spellwork/Alarm.java
+++ app/src/main/java/com/anumalm/spellwork/Alarm.java
@@ -5,8 +5,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 
@@ -56,13 +58,18 @@ public class Alarm extends BroadcastReceiver {
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("Move it buddy")
                         .setContentText("Tiem to get ur move to the groove boi!");
+        // Set vibration to notification
+        mBuilder.setVibrate(new long[] {1000,1000});
         mBuilder.setContentIntent(contentIntent);
-        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+
+        // Set alarm sound to an annoying chirp sound
+        Uri alarmSound = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.alarm);
+        mBuilder.setSound(alarmSound);
         mBuilder.setAutoCancel(true);
+
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(1, mBuilder.build());
 
     }
-
 }
