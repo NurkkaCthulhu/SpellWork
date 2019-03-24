4c3e9e5ddce992996f17c6008082e80074410d73
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-24 20:16:24 +0200

    Create Alarm.java
    Currently sends a notification every minute. Will be fixed on later commit.

---
+++ app/src/main/java/com/anumalm/spellwork/Alarm.java
@@ -0,0 +1,68 @@
+package com.anumalm.spellwork;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
+
+import com.anumalm.spellwork.utilities.Debug;
+
+public class Alarm extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, ":mywakelocktag");
+        wl.acquire();
+
+        Debug.log("ALARM", "Alarm/onReceive", "We are here!", 1);
+
+        showNotification(context);
+
+        wl.release();
+    }
+
+    public void setAlarm(Context context)
+    {
+        Debug.log("ALARM", "Alarm/setAlarm", "Setting alarm...", 1);
+
+        AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, Alarm.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 1, pi); // Millisec * Second * Minute
+    }
+
+    public void cancelAlarm(Context context)
+    {
+        Intent intent = new Intent(context, Alarm.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
+    }
+
+    private void showNotification(Context context) {
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Move it buddy")
+                        .setContentText("Tiem to get ur move to the groove boi!");
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
+    }
+
+}
