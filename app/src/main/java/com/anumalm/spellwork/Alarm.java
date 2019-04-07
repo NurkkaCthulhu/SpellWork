abfe1f275a2dc2fbb2db83d706f0cbc7879a9aff
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-07 15:56:48 +0300

    Alarm.java: Change notifications to work on Android Oreo ->.

--- app/src/main/java/com/anumalm/spellwork/Alarm.java
+++ app/src/main/java/com/anumalm/spellwork/Alarm.java
@@ -1,12 +1,17 @@
 package com.anumalm.spellwork;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
 import android.net.Uri;
+import android.os.Build;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 
@@ -38,7 +43,7 @@ public class Alarm extends BroadcastReceiver {
 
         Debug.log("ALARM", "Alarm/onReceive", "Time to send notification!", 1);
 
-        showNotification(context);
+        showNotification(context, intent);
 
         wl.release();
     }
@@ -78,27 +83,35 @@ public class Alarm extends BroadcastReceiver {
      *
      * @param context           The Context in which the receiver is running.
      */
-    private void showNotification(Context context) {
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                new Intent(context, MainActivity.class), 0);
+    private void showNotification(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("Move it buddy")
-                        .setContentText("Tiem to get ur move to the groove boi!");
-        // Set vibration to notification
-        mBuilder.setVibrate(new long[] {1000,1000});
-        mBuilder.setContentIntent(contentIntent);
+        int notificationId = 1;
+        String channelId = "channel-01";
+        String channelName = "Channel Name";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
 
-        // Set alarm sound to an annoying chirp sound
-        Uri alarmSound = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.alarm);
-        mBuilder.setSound(alarmSound);
-        mBuilder.setAutoCancel(true);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(
+                    channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(1, mBuilder.build());
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("New workout!")
+                .setContentText("Time to get up u lazy lad");
 
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        notificationManager.notify(notificationId, mBuilder.build());
     }
+
+
 }
