ee59df52205e9b356c1ac355bae95187b44f37af
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-08 02:43:13 +0300

    WorkoutActivity.java: Add random workout and test.

--- app/src/main/java/com/anumalm/spellwork/WorkoutActivity.java
+++ app/src/main/java/com/anumalm/spellwork/WorkoutActivity.java
@@ -4,6 +4,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.anumalm.spellwork.utilities.Debug;
@@ -34,6 +37,10 @@ public class WorkoutActivity extends AppCompatActivity {
 
         t = findViewById(R.id.workoutText);
         getSettings();
+
+        Workout wo = new JumpingJacks(10, "Jumping jacks", true, R.drawable.button_back);
+        ImageView iw = findViewById(R.id.workoutImg_top);
+        iw.setImageResource(((JumpingJacks) wo).getGraphic());
 
     }
 
