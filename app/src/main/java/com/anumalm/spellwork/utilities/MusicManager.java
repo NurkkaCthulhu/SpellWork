5d05823268a2f7ee697e11a1a9d2805831f152f3
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-21 00:01:39 +0300

    MusicManager.java: Add debugs and method to check if music should really be paused.
    Otherwise music gets paused for a bit when changing activities.

--- app/src/main/java/com/anumalm/spellwork/utilities/MusicManager.java
+++ app/src/main/java/com/anumalm/spellwork/utilities/MusicManager.java
@@ -8,9 +8,12 @@ import com.anumalm.spellwork.R;
 public class MusicManager {
     private static boolean currentlyPlaying = false;
     private static MediaPlayer musicPlayer;
+    private static boolean pause = true;
 
     public static void createMusicPlayer(Context context) {
+        Debug.log("MUSIC", "MusicManager/create", "music manager create method", 1);
         if(musicPlayer == null) {
+            Debug.log("MUSIC", "MusicManager/create", "music manager was null", 2);
             musicPlayer = MediaPlayer.create(context, R.raw.bgmusic);
             musicPlayer.setLooping(true);
             musicPlayer.setVolume(100, 100);
@@ -18,23 +21,38 @@ public class MusicManager {
     }
 
     public static void playMusic() {
+        Debug.log("MUSIC", "MusicManager/play", "trying to play music", 1);
         if(!currentlyPlaying) {
+            Debug.log("MUSIC", "MusicManager/play", "music starting to play", 2);
             currentlyPlaying = true;
             musicPlayer.start();
         }
     }
 
     public static void pauseMusic() {
-        if(currentlyPlaying) {
+        Debug.log("MUSIC", "MusicManager/pause", "music pause method", 1);
+        if(currentlyPlaying && pause) {
+            Debug.log("MUSIC", "MusicManager/pause", "music paused", 2);
+            currentlyPlaying = false;
             musicPlayer.pause();
         }
     }
 
     public static void destroyMusicPlayer() {
+        Debug.log("MUSIC", "MusicManager/destroy", "music player destroy method", 1);
         if(musicPlayer != null) {
+            Debug.log("MUSIC", "MusicManager/destroy", "music player was not null and will destroy", 2);
             currentlyPlaying = false;
             musicPlayer.stop();
             musicPlayer.release();
         }
+    }
+
+    public static boolean isPaused() {
+        return pause;
+    }
+
+    public static void setPause(boolean pause) {
+        MusicManager.pause = pause;
     }
 }
