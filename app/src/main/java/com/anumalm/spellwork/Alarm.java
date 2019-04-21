24dcf5391a3d3b66ca1ddfc72c98bf9f16db1648
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-21 23:40:07 +0300

    Alarm.java: Remove minutes get from user settings. Back to static minutes.

--- app/src/main/java/com/anumalm/spellwork/Alarm.java
+++ app/src/main/java/com/anumalm/spellwork/Alarm.java
@@ -22,7 +22,6 @@ import com.anumalm.spellwork.utilities.Debug;
  */
 public class Alarm extends BroadcastReceiver {
 
-    private int minutes;
     /**
      * Overrides BroadcastReceiver's onReceive-method.
      *
@@ -34,12 +33,11 @@ public class Alarm extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, ":swwakelocktag");
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, ":mywakelocktag");
         wl.acquire(1000);
 
         Debug.log("ALARM", "Alarm/onReceive", "Time to send notification!", 1);
 
-        getMinutes(context);
         showNotification(context, intent);
 
         wl.release();
@@ -52,16 +50,12 @@ public class Alarm extends BroadcastReceiver {
      */
     public void setAlarm(Context context) {
         Debug.log("ALARM", "Alarm/setAlarm", "Setting alarm...", 1);
+        SharedPreferences settings = context.getSharedPreferences("UserSettings", 0);
 
         AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, Alarm.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * minutes, pi); // Millisec * Second * Minute
-    }
-
-    private void getMinutes(Context context) {
-        SharedPreferences settings = context.getSharedPreferences("UserSettings", 0);
-        minutes = settings.getInt("workouttimer", 30);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 1, pi); // Millisec * Second * Minute
     }
 
     /**
