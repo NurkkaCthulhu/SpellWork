ca63e0e4f793054e6aac99ca51eab57e548a380c
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-22 19:17:30 +0300

    Alarm.java: Change alarm type.

--- app/src/main/java/com/anumalm/spellwork/Alarm.java
+++ app/src/main/java/com/anumalm/spellwork/Alarm.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.PowerManager;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import com.anumalm.spellwork.utilities.Debug;
 
@@ -56,7 +57,9 @@ public class Alarm extends BroadcastReceiver {
         AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, Alarm.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * timerMinutes, pi); // Millisec * Second * Minute
+        am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 1000 * 60 * timerMinutes,
+                1000 * 60 * timerMinutes, pi);
     }
 
     /**
