1200a87bd395325a6638613d391f0b508233a196
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-22 22:30:28 +0300

    SplashActivity.java: Add timer to move to MainActivity after 2 seconds.

--- app/src/main/java/com/anumalm/spellwork/SplashActivity.java
+++ app/src/main/java/com/anumalm/spellwork/SplashActivity.java
@@ -1,13 +1,32 @@
 package com.anumalm.spellwork;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+/**
+ * Simple splash activity to show app title.
+ */
+public class SplashActivity extends SpellworkActivity {
+
+    /**
+     * Overrides AppCompatActivity's onCreate method.
+     *
+     * @param savedInstanceState            Bundle that's somewhere in AppCompatActivity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Set timer for 2 sec so user can see the app's name.
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 2000);
     }
 }
